3c98c95370f2117eeba1b1e0c7141208412e034a
leonanton20051609-source <leonanton20051609@gmail.com>
2025-09-22 19:24:02 +0200

    Add files via upload

---
+++ src/main/java/com/example/vdaddon/VampireDiariesAddon.java
@@ -0,0 +1,10 @@
+package com.example.vdaddon;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod("vdaddon")
+public class VampireDiariesAddon {
+    public VampireDiariesAddon() {
+        System.out.println("Vampire Diaries Addon geladen!");
+    }
+}
